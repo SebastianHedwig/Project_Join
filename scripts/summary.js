4d933b418bcf84c7b2c584f6ea21b942c43fc529
SaschaKessler-dev <sascha.k1809@gmail.com>
2025-10-12 16:18:03 +0200

    implement function greetingHeader

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,14 +1,37 @@
 
 async function init() {
    await getData()
+    greetings();
+}
+
+
+function greetings() {
+    greetingHeader();
     greetingName();
 }
 
 
 function greetingName() {
-   let greetingName = document.getElementById('greeting');
-   if (users.length >= 0) {
+   let greetingName = document.getElementById('greeting-name');
+   greetingName.innerHTML = "";
+   if (users.length > 0) {
     let userName = users[0]
     greetingName.innerHTML = `${userName.firstName} ${userName.lastName}` 
    }
+}
+
+
+function greetingHeader() {
+    let greetingHeader = document.getElementById('greeting-header');
+    
+    let hour = new Date().getHours();
+    let greetingText = "";
+    if (hour < 12) {
+        greetingText = "Good morning,"
+    } else if (hour < 18) {
+        greetingText = "Good afternoon,"
+    } else {
+        greetingText = "Good evening,"
+    }
+    greetingHeader.textContent = greetingText;
 }
