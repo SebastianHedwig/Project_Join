426927e5804794e45db00e6f72675ea07ea96629
SaschaKessler-dev <sascha.k1809@gmail.com>
2025-10-21 20:16:42 +0200

    implement task priority img in taskTemplate

--- templates/tpl-add-task.js
+++ templates/tpl-add-task.js
@@ -1,26 +1,34 @@
 function getTasksTemplate(index) {
-    return `<div class="task" onclick="renderTaskInfoDlg()">
-                                    <span class="task__category">${tasks[index].category}</span>
-                                    <div class="task__content-metadata-box">
-                                        <span class="task__title">${tasks[index].title}</span>
-                                        <span class="task__description">${tasks[index].description}</span>
-                                    </div>
-                                    <div class="task__subtasks-and-progressbar-box">
-                                        <span class="task__progressbar" style="--progress: 33.3333%;"></span>
-                                        <span class="task__subtasks"> 1 / 3 Subtasks</span>
-                                    </div>
-                                    <div class="task__assignment-and-priority-box">
-                                        <div class="task__assignments">
-                                            <!-- user images nur zur veranschaulichung. wird später mit JS gerendert -->
-                                            <img class="user-img" id="" src="../assets/img/user-img-anna.svg"
-                                                alt="User Image or initials">
-                                            <img class="user-img" id=""
-                                                src="../assets/img/user-img-david.svg" alt="User Image or initials">
-                                            <img class="user-img" id=""
-                                                src="../assets/img/user-img-sofia.svg" alt="User Imare or initials">
-                                        </div>
-                                        <div class="task__priority">
-                                            <img id="priority" class="task_priority" src="../assets/img/priority-medium.svg"
-                                                alt="image of prioritylevel">
-                                        </div>`
+  return `
+    <div class="task" onclick="renderTaskInfoDlg()">
+      <span class="task__category">${tasks[index].category}</span>
+      <div class="task__content-metadata-box">
+        <span class="task__title">${tasks[index].title}</span>
+        <span class="task__description">${tasks[index].description}</span>
+      </div>
+      <div class="task__subtasks-and-progressbar-box">
+        <span class="task__progressbar" style="--progress: 33.3333%;"></span>
+        <span class="task__subtasks">1 / 3 Subtasks</span>
+      </div>
+      <div class="task__assignment-and-priority-box">
+        <div class="task__assignments">
+          <img class="user-img" src="../assets/img/user-img-anna.svg" alt="User Image">
+          <img class="user-img" src="../assets/img/user-img-david.svg" alt="User Image">
+          <img class="user-img" src="../assets/img/user-img-sofia.svg" alt="User Image">
+        </div>
+        <div class="task__priority">
+          <img 
+            src="${
+              tasks[index].priority === 'urgent'
+                ? '../assets/img/priority-urgent.svg'
+                : tasks[index].priority === 'medium'
+                ? '../assets/img/priority-medium.svg'
+                : '../assets/img/priority-low.svg'
+            }"
+            alt="${tasks[index].priority} priority icon"
+          >
+        </div>
+      </div>
+    </div>
+  `;
 }
