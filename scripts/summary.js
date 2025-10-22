d141fbd21b5fc851f722689a78afbb0a945434a0
SaschaKessler-dev <sascha.k1809@gmail.com>
2025-10-22 19:16:56 +0200

    feat(summary): implement task counting functions (countToDos and countingLoop)

--- scripts/summary.js
+++ scripts/summary.js
@@ -3,6 +3,7 @@ async function initSummary() {
    await getData();
     greetings();
     currentDate();
+    countToDos();
 }
 
 
@@ -50,4 +51,37 @@ function currentDate() {
 
     let completeDate = `${month} ${day}, ${year}`;
     date.textContent = completeDate;
+}
+
+function countToDos() {
+    let awaitingFeedback = document.getElementById('awaiting-feedback');
+    let inProgress = document.getElementById('in-progress-count');
+    let tasksInBoard = document.getElementById('tasks-count');
+    let done = document.getElementById('done-count');
+    let toDos = document.getElementById('to-do-count');
+    let urgent = document.getElementById('urgent-count');
+    toDos.innerHTML = "0";
+    done.innerHTML = "0";
+    urgent.innerHTML = "0";
+    tasksInBoard.innerHTML = "0";
+    inProgress.innerHTML = "0";
+    awaitingFeedback.innerHTML = "0";
+    for (let index = 0; index < tasks.length; index++) {
+        countingLoop(index, {toDos, done, urgent, inProgress, awaitingFeedback, tasksInBoard});
+        tasksInBoard.innerHTML = index +1;
+    }  
+}
+
+function countingLoop(index, counters) {
+    if (tasks[index].taskState === "toDo") {
+            counters.toDos.innerHTML = Number(counters.toDos.innerHTML) +1;
+        } if (tasks[index].taskState === "done") {
+            counters.done.innerHTML = Number(counters.done.innerHTML) +1;
+        } if (tasks[index].priority === "urgent") {
+            counters.urgent.innerHTML = Number(counters.urgent.innerHTML) +1;
+        } if (tasks[index].taskState === "inProgress") {
+            counters.inProgress.innerHTML = Number(counters.inProgress.innerHTML) +1;
+        } if (tasks[index].taskState === "awaitingFeedback") {
+            counters.awaitingFeedback.innerHTML = Number(counters.awaitingFeedback.innerHTML) +1;
+        }   
 }
